b4d9a5c4d578ae8178125cb8b4aa528f8723bc3b
anna <anna@sayf.su>
2022-12-15 23:18:23 +0300

    commit 6

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
+        task6();
+        task7();
     }
 
     public static void task1() {
@@ -48,4 +51,60 @@ public class Main {
         }
         System.out.println();
     }
-}
+    public static void task5() {
+        System.out.println("Задача 5");
+        int saturday = 1;
+        for (; saturday <= 31; saturday+=7) {
+            int sunday = saturday + 1;
+            System.out.println("Суббота - " + saturday + " число, воскресенье - " + sunday + " число");
+        }
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+        int iteration_count = 0;
+        for (int ticket = 1; ticket <= 999_999; ticket++) {
+        int a = ticket / 100_000;
+        int b = (ticket % 100_000) / 10000;
+        int c = (ticket % 10000) / 1000;
+        int d = (ticket % 1000) / 100;
+        int e = (ticket % 100) / 10;
+        int f = ticket % 10;
+                if ( a + b + c == d + e + f) {
+                iteration_count++;
+            }
+
+        }
+            System.out.println(iteration_count);
+        }
+    private static void task7() {
+        System.out.println("Задача 7");
+        int result = 0;
+        int first = 1;
+        int last = 999_999;
+        for (int current = first; current <= last; current++) {
+            int firstPart = current / 1000;
+            int lastPart = current % 1000;
+            if (firstPart == 0 || lastPart == 0) {
+                continue;
+            }
+            int firstSum = calculateSumOfDigits(firstPart);
+            int lastSum = calculateSumOfDigits(lastPart);
+            if (firstSum == lastSum) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static int calculateSumOfDigits(int num) {
+        int result = 0;
+
+        while (num > 0) {
+            result += num % 10;
+            num /= 10;
+        }
+
+        return result;
+    }
+
+    }
